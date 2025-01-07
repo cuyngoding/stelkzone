9e58ed459ea3a74563be31c711e3905099560738
cuyngoding <arthawanpratama1@gmail.com>
2025-01-08 00:11:04 +0800

    React JS, Vite, and Laravel project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,38 +1,46 @@
+import { useState } from "react";
 import "./Navbar.css";
 import { LuMenu } from "react-icons/lu";
+import { GoHomeFill } from "react-icons/go";
+import { FaUser  } from "react-icons/fa";
+import { FaUsersGear } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import PhotoProfile from "../assets/user-profile.png";
 
 function Navbar() {
   const navigate = useNavigate();
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
   const handleLogout = () => {
-    
     navigate('/'); // navigasi ke halaman login
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
   };
 
   return (
     <>
-    <div className="nav-container">
-      <button className="button-menu">
-        <LuMenu className="menu-icon" />
-      </button>
-      <div className="profile">
-        <img className="user-photo" src={PhotoProfile} alt="User Profile" />
-        <h3 className="user-name">Nama User</h3>
+      <div className="nav-container">
+        <button className="button-menu" onClick={toggleSidebar}>
+          <LuMenu className="menu-icon" />
+        </button>
+        <div className="profile">
+          <img className="user-photo" src={PhotoProfile} alt="User  Profile" />
+          <h3 className="user-name">Nama User</h3>
+        </div>
+        <button className="btn-logout" onClick={handleLogout}>
+          Log out
+        </button>
       </div>
-      <button className="btn-logout" onClick={handleLogout}>
-        Log out
-      </button>
-    </div>
-    
-    <div className="sidebar-offcanvas-container">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Eskul lainnya</a></li>
-      </ul>
-    </div>
+
+      <div className={`sidebar-offcanvas-container ${isSidebarVisible ? 'visible' : ''}`}>
+        <ul>
+          <li><a href="#"> <span><GoHomeFill/></span> Home</a></li>
+          <li><a href="#"> <span><FaUser /></span> Profile</a></li>
+          <li><a href="#"><span className="icon-navigation-eskul"><FaUsersGear/></span> Eskul lainnya</a></li>
+        </ul>
+      </div>
     </>
   );
 }
