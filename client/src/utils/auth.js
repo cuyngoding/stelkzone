715acf1fe427ac0b88469237d32fee0ab9b97866
FabioGanteng <aldrin.leimena123@gmail.com>
2025-02-24 16:31:47 +0800

    error fix

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api"; // Sesuaikan dengan backend Laravel
+const API_URL = "http://127.0.0.1:8000/api"; // Sesuaikan dengan URL backend Laravel
 
 export const login = async (email, password) => {
   try {
